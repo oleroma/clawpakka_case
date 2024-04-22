aa2817b2476faeb806091e86506e4766075414fb
Marcos Diaz <marcos@inputlabs.io>
2024-04-22 14:25:48 +0300

    Build in the correct planes

--- build123d/wheel.py
+++ build123d/wheel.py
@@ -6,19 +6,19 @@ from build123d import (
 )
 
 # Wheel
-INDENTS = 24
+WHEEL_INDENTS = 24
 WHEEL_RADIUS_OUTER = 10.75
 WHEEL_RADIUS_INNER = 10.25
 WHEEL_WIDTH = 6.75
-CHAMFER = 0.75
+WHEEL_CHAMFER = 0.75
 
 # Core
 CORE_RADIUS = 8
 CORE_TOLERANCE = 0.1
-RIGHT_CONTACT_LEN = 1.8
-RIGHT_CONTACT_RADIUS = 2
+RIGHT_PADDING_LEN = 1.8
+RIGHT_PADDING_RADIUS = 2
 HEX_DIAMETER = 2.05
-HEX_LEN = 3.5
+HEX_LEN = 5.3
 LEFT_AXLE_DIAMETER = HEX_DIAMETER * cos(pi / 6)
 LEFT_AXLE_LEN = 5
 
@@ -35,33 +35,33 @@ CENTER = (0, 0)
 PRINTBED_PLANE = Plane.XZ.offset(LEFT_AXLE_DIAMETER / 2)
 
 # Core sketch.
-with BuildSketch() as core_sketch:
+with BuildSketch(Plane.XZ) as core_sketch:
     Circle(CORE_RADIUS)
     split(bisect_by=PRINTBED_PLANE, keep=Keep.BOTTOM)
 
 # Wheel part.
 with BuildPart() as wheel:
-    with BuildSketch() as s:
+    with BuildSketch(Plane.XZ) as s:
         # Create a single indent chevron.
         with BuildLine(mode=Mode.PRIVATE) as l:
             # Create indent line.
-            angle = 180 / INDENTS
+            angle = 180 / WHEEL_INDENTS
             a = (WHEEL_RADIUS_OUTER, 0)
             b = PolarLine(CENTER, WHEEL_RADIUS_INNER, angle, mode=Mode.PRIVATE) @ 1
             Line(a, b)
             # Mirror line into a chevron.
-            mirror(about=Plane.XZ)
+            mirror(about=Plane.YZ)
         # Repeat chevrons around a circle.
-        with PolarLocations(0, INDENTS):
+        with PolarLocations(0, WHEEL_INDENTS):
             add(l.line)
         make_face()
     # Make 3D.
     extrude(amount=WHEEL_WIDTH)
     # Chamfer.
-    edge_list = edges().filter_by(Axis.Z, reverse=True)
-    chamfer(edge_list, CHAMFER)
+    edge_list = edges().filter_by(Axis.Y, reverse=True)
+    chamfer(edge_list, WHEEL_CHAMFER)
     # Remove the core.
-    with BuildSketch() as s:
+    with BuildSketch(Plane.XZ) as s:
         add(core_sketch.sketch)
         offset(amount=CORE_TOLERANCE, kind=Kind.INTERSECTION)
     extrude(amount=WHEEL_WIDTH, mode=Mode.SUBTRACT)
@@ -69,26 +69,27 @@ with BuildPart() as wheel:
 # Core part.
 with BuildPart() as core:
     # Main core.
-    add(core_sketch.sketch)
+    with BuildSketch(Plane.XZ):
+        add(core_sketch.sketch)
     extrude(amount=WHEEL_WIDTH)
-    # Right contact.
-    with BuildSketch(Plane.XY.offset(WHEEL_WIDTH)) as s:
-        Circle(RIGHT_CONTACT_RADIUS)
-        split(bisect_by=PRINTBED_PLANE, keep=Keep.BOTTOM)
-    extrude(amount=RIGHT_CONTACT_LEN)
     # Right axle (hexagon).
-    with BuildSketch(Plane.XY.offset(WHEEL_WIDTH + RIGHT_CONTACT_LEN)) as s:
+    with BuildSketch(Plane.XZ.offset(WHEEL_WIDTH)) as s:
         RegularPolygon(HEX_DIAMETER / 2, 6)
     extrude(amount=HEX_LEN)
+    # Right padding.
+    with BuildSketch(Plane.XZ.offset(WHEEL_WIDTH)) as s:
+        Circle(RIGHT_PADDING_RADIUS)
+        split(bisect_by=PRINTBED_PLANE, keep=Keep.BOTTOM)
+    extrude(amount=RIGHT_PADDING_LEN)
     # Left axle.
-    with BuildSketch() as s:
+    with BuildSketch(Plane.XZ) as s:
         Circle(LEFT_AXLE_DIAMETER / 2)
     # Make 3D.
     extrude(amount=-LEFT_AXLE_LEN)
 
 # Holder part.
 with BuildPart() as holder:
-    with BuildSketch(Plane.XY) as s:
+    with BuildSketch(Plane.XZ) as s:
         offset = Location((0, HOLDER_AXLE_OFFSET_FROM_TOP))
         with BuildLine(offset) as l:
             # Create half polygon.

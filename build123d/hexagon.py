7b5703e2c35a724d133a39f2031ec8beb97958e2
Marcos Diaz <marcos@inputlabs.io>
2025-01-16 11:05:36 +0200

    Prevent hex wrong angle insertion

--- build123d/hexagon.py
+++ build123d/hexagon.py
@@ -1,69 +1,73 @@
-from build123d import (
-    BuildPart, BuildSketch, BuildLine, Polyline, Circle, Plane, Locations, Mode, Location,
-    mirror, make_face, extrude, add)
-
+from build123d import *
 import math
 
-try:
-    from ocp_vscode import show_object
-except ModuleNotFoundError:
-    pass
+BASE_X_LEN = 36
+BASE_Y_LEN = 40
+BASE_Z_LEN = 1.9
+BASE_Y_SIDE = 20
 
+HOLE_RADIUS = 5
+HOLE_DISTANCE_FROM_CENTER = 12
 
-# top hex
-HEX_TOP_HEIGHT = 1.9
-HEX_DEPTH = 40
-HEX_PTS = ( ## only half => mirror
-    (0, 0),
-    (18, 10),
-    (18, 30),
-    (0, HEX_DEPTH))
+TABS_Z_LEN = 4
 
-HEX_HOLE_LOCATIONS=(
-    (0, 8),
-    (0, 32),
-    (12, 20),
-    (-12, 20))
-
-hex_hole_radius=5
-
-
-# trapezoid
-TRAPEZ_WIDTH = 2.758 ## tight: 2.8
+TRAPEZ_WIDTH = 2.758  ## Tight version was 2.8.
+TRAPEZ_HEIGHT = 18
+TRAPEZ_TIP_WIDTH = 2.2
 TRAPEZ_PTS = (
     (0, 0),
-    (math.sqrt(10 * 10 + 10 * 10), 0), # diagonal of width (10) and depth (10)
-    (2.2, 18 - HEX_TOP_HEIGHT),
-    (0, 18 - HEX_TOP_HEIGHT))
-
+    (math.sqrt(10**2 + 10**2),         0),  # Diagonal of 10x10 (Blender legacy).
+    (TRAPEZ_TIP_WIDTH, TRAPEZ_HEIGHT - BASE_Z_LEN),
+    (0,                TRAPEZ_HEIGHT - BASE_Z_LEN),
+)
 
 with BuildPart() as chex:
+    # Base.
     with BuildSketch():
-        with BuildLine() as top_outline:
-           line = Polyline(HEX_PTS)
-           mirror(top_outline.line, about=Plane.YZ)
+        with BuildLine():
+            Polyline([
+                (0,            BASE_Y_LEN/2),
+                (BASE_X_LEN/2, BASE_Y_SIDE/2),
+                (BASE_X_LEN/2, 0),
+            ])
+            mirror(about=Plane.YZ)
+            mirror(about=Plane.XZ)
         make_face()
+        # Holes.
+        locations = [
+            (0, HOLE_DISTANCE_FROM_CENTER),
+            (0, -HOLE_DISTANCE_FROM_CENTER),
+            (HOLE_DISTANCE_FROM_CENTER, 0),
+            (-HOLE_DISTANCE_FROM_CENTER, 0),
+        ]
+        with Locations(locations):
+            Circle(HOLE_RADIUS, mode=Mode.SUBTRACT)
+    extrude(amount=BASE_Z_LEN)
 
-        with Locations(HEX_HOLE_LOCATIONS):
-            Circle(hex_hole_radius, mode=Mode.SUBTRACT)
-
-    extrude(amount=HEX_TOP_HEIGHT)
-
-    with BuildPart(mode=Mode.PRIVATE) as bottom_pt:
+    # Trapezoid.
+    with BuildPart(mode=Mode.PRIVATE) as trapezoid_pt:
         plane = Plane.XZ.offset(-TRAPEZ_WIDTH / 2)
         with BuildSketch(plane):
-            with BuildLine() as bottom_outline:
-                line = Polyline(TRAPEZ_PTS)
-                mirror(bottom_outline.line, about=Plane.YZ)
-
+            with BuildLine():
+                Polyline(TRAPEZ_PTS)
+                mirror(about=Plane.YZ)
             make_face()
         extrude(amount=TRAPEZ_WIDTH)
+    with Locations((0, 0, BASE_Z_LEN)):
+        add(trapezoid_pt, rotation=(0, 0, -45))
+        add(trapezoid_pt, rotation=(0, 0, 45))
 
-    with Locations(Location((0, HEX_DEPTH / 2, HEX_TOP_HEIGHT))):
-        add(bottom_pt, rotation=(0, 0, -45))
-        add(bottom_pt, rotation=(0, 0, 45))
+    # Tabs (to prevent wrong insertion).
+    with BuildSketch(Plane.XY.offset(TRAPEZ_HEIGHT)):
+        cos45 = math.cos(math.radians(45))
+        width = (cos45 * TRAPEZ_TIP_WIDTH) + (cos45 * TRAPEZ_WIDTH/2)
+        height = TRAPEZ_TIP_WIDTH - TRAPEZ_WIDTH
+        Rectangle(width*2, height*2)
+    extrude(amount=-TABS_Z_LEN)
 
 
-if __name__ in ['__main__', 'temp']:
+if __name__ == '__main__':
+    from common.vscode import show_object
     show_object(chex)
     print(f"Volume: {chex.part.volume}")
+    # export_stl(chex.part, 'stl/test_hex.stl')
